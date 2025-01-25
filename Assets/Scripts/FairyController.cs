f5ceb9b0e26ce61d0fda41d335b29a0ef9c6ed86
Krows-MJ <lukakrows@gmail.com>
2025-01-25 11:24:22 -0400

    modificación de los Axis

--- Assets/Scripts/FairyController.cs
+++ Assets/Scripts/FairyController.cs
@@ -7,7 +7,7 @@ public class FairyController : MonoBehaviour
     private Rigidbody rb;
     [Header("Variables")]
     [SerializeField][Range(0f, 10f)] private float speed;
-    private float hmov, vmov;
+    public float hmov, vmov;
     private Vector3 vectorMove;
     [Header("Proyectil/Burbuja")]
     [SerializeField] private GameObject Bulled;
@@ -24,13 +24,14 @@ public class FairyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //bulletBubble
         if (Input.GetKeyDown(KeyCode.Space)){
             Debug.Log("Lanza burbuja");
         }
     }
     private void FixedUpdate(){
-        hmov= Input.GetAxis("Horizontal");
-        vmov= Input.GetAxis("Vertical");
+        hmov= Input.GetAxis("HorizontalF");
+        vmov= Input.GetAxis("VerticalF");
         vectorMove= new Vector3(hmov, vmov, 0f);
         vectorMove= Vector3.ClampMagnitude(vectorMove, 1);
         rb.velocity= vectorMove * speed;
